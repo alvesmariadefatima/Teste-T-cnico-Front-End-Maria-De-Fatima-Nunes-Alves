4a80f92ba51e20e65846f3bf91f0a97214226aa1
Maria de Fátima <mnunesalves334@gmail.com>
2024-06-27 15:50:53 -0300

    Ajustes nas rotas de redirecionamento da aplicação

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
-import CarrinhoCompras from './components/CarrinhoCompras/CarrinhoCompras';
+import Carrinho from './components/Carrinho/Carrinho';
 
 function AppRoutes() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} index />
-                <Route path="/carrinho" element={<CarrinhoCompras />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/carrinho" element={<Carrinho />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default AppRoutes;
