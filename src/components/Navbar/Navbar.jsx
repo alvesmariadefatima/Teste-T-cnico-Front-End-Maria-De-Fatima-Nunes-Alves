5941f9fede2b93d14f25f13177408bc5db747691
Maria de Fátima <mnunesalves334@gmail.com>
2024-06-27 15:55:59 -0300

    Ajustes na estrutura do menu de navegação

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,20 @@
 import styles from './Navbar.module.css';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
         <nav className={styles.menu}>
             <div className={styles.containerLogotipo}>
-                <img src="/icone-nextgen-gaming.png" alt="Logotipo da loja NextGen Gaming" />
+                <Link to="/" className={styles.logoLink}>
+                    <img src="/icone-nextgen-gaming.png" alt="Logotipo da loja NextGen Gaming" className={styles.logoImg} />
+                    <p className={styles.nomeSite}>NextGen Gaming</p>
+                </Link>
             </div>
-            <p className={styles.nomeSite}>NextGen Gaming</p>
 
             <div className={styles.containerCarrinho}>
-                <img src="/carrinho.png" alt="Ícone do Carrinho de Compras" />
+                <Link to="/carrinho">
+                    <img src="/carrinho.png" alt="Ícone do Carrinho de Compras" className={styles.carrinhoIcon} />
+                </Link>
             </div>
         </nav>
     );
