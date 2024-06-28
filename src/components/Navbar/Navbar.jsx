e4864bad62964145e4782546355fddc638ef07b8
Maria de Fátima Nunes Alves <94319702+alvesmariadefatima@users.noreply.github.com>
2024-06-28 15:23:41 -0300

    Remoção do ícone do carrinho de compras do menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,9 @@
 import styles from './Navbar.module.css';
-import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
         <nav className={styles.menu}>
             <p className={styles.nomeSite}>NextGen Gaming</p>
-
-            <div className={styles.containerCarrinho}>
-                <Link to="/carrinho">
-                    <img src="/carrinho.png" alt="Ícone do Carrinho de Compras" className={styles.carrinhoIcon} />
-                </Link>
-            </div>
         </nav>
     );
 }
